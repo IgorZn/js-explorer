dbba2fadd074b2f07dab366f68d625571157b9a3
igor.znamensky <igor.znamensky@gmail.com>
2024-03-15 10:28:58 +0300

    next 095

---
+++ server/src/server.js
@@ -0,0 +1,9 @@
+import http from "http"
+import app from "./app.js";
+
+const PORT = process.env.PORT || 8000
+
+const server = http.createServer(app)
+server.listen(PORT, () => {
+    console.log(`Server running at port: ${PORT}`)
+})
